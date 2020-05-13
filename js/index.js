34f7a9dd6e2c050c9d915193ee0164791f97fde1
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-05-13 20:24:43 +0200

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('button');
-const article = document.querySelector('article');
-
-button.addEventListener('click', function () {
-    const el = document.createElement('em');
-    el.innertext = 'aye';
-    document.body.prepend(el);
-})
