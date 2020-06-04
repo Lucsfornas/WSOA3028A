572ad8cc8044212b0b79a29063cae43f83ba0223
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-06-04 17:56:28 +0200

    Update Menu.js

--- WSOA3028A/js/Menu.js
+++ WSOA3028A/js/Menu.js
@@ -4,14 +4,18 @@ const menuData = [
     { title: 'Characters Blog', link: '/CBlog' },
 ];
 const initialiseMenu = () => {
-    const nav = document.querySelector('nav');
+    const nav = document.querySelector('.menu');
+
     const ul = document.createElement('Ul');
+
     for (let item of menuItems) {
         const li = document.createElement('li');
         const a = document.createElement('a');
         a.innerText = item.title;
         a.href = item.link;
+
         li.appendChild(a);
+
         ul.appendChild(li);
 
     }
