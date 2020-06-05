881b79425f53e19464c1480484110093c97938d7
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-06-05 13:25:30 +0200

    Create Menu.js

---
+++ JavaScript/Menu.js
@@ -0,0 +1,23 @@
+const menuItems = [
+    { title: 'Home', link: './index.html' },
+    { title: 'personal Blog', link: '/WSOA3028A/personalBlog.html' },
+    { title: 'Characters Blog', link: '/WSOA3028/CharactersBlog' },
+    { title: 'About', link: '/index.html' },
+];
+const initialiseMenu = () => {
+    const nav = document.querySelector('Menu');
+
+    const ul = document.createElement('ul');
+
+    for (let item of menuItems) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.innerText = item.title;
+        a.href = item.link;
+        li.appendChild(a);
+        ul.appendChild(li);
+
+    }
+    nav.appendChild(ul);
+};
+document.addEventListener('DomContentLoaded', () => initialiseMenu());
