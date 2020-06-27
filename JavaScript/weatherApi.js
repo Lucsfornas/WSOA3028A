b87bdc1144eaf0931e7ed902cf5fbf4591f26110
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-06-28 00:21:43 +0200

    Create weatherApi.js

---
+++ JavaScript/weatherApi.js
@@ -0,0 +1,25 @@
+var button = document.querySelector('./button')
+var inputvalue = document.querySelector('./inputvalue')
+var name = document.querySelector("./name")
+var desc = document.querySelector("./desc")
+var temp = document.querySelector("./temp")
+
+
+
+button.addEventListener('click', function () {
+    fetch('api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&APPID=8a71da0d39cee6e47fe8472e84a233a2')
+        .then(responce => responce.json())
+        .then(data => {
+            var namevalue = data['name'];
+            var tempvalue = data['main']['temp'];
+            var descvalue = data['weather'][0]['description'];
+
+            name.innerHTML = namevalue;
+            temp.innerHTML = tempvalue;
+            desc.innerHTML = descvalue;
+        })
+
+
+
+        .catch(err => alert("wrong city name"))
+})
