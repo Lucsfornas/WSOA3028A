b53c79df7bd7711f4ffaf628c24aac8b8ed724a7
Lucsfornas <60962295+Lucsfornas@users.noreply.github.com>
2020-06-28 18:12:27 +0200

    Update weatherApi.js

--- JavaScript/weatherApi.js
+++ JavaScript/weatherApi.js
@@ -8,15 +8,15 @@ window.addEventListener('load', () => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const proxy = 'http://cors-anywhere.herouapp.com/'
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=8a71da0d39cee6e47fe8472e84a233a2`;
+
+            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=8a71da0d39cee6e47fe8472e84a233a2`;
 
             fetch(api)
                 .then(responce => {
                     return responce.json();
                 })
                 .then(data => {
-
+                    console.log(data);
                 });
         });
 
